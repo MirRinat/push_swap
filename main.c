227b7b3771226beeb8afe59ad868ccb693c9ea79
Bonebreaker Glinda <bglinda@mi-g6.21-school.ru>
2020-01-17 19:05:55 +0300

    заношу все в структуру,но временами segmentation

--- main.c
+++ main.c
@@ -1,60 +1,47 @@
 #include <stdio.h>
-#include "libft/libft.h"
+#include "push_swap.h"
+
+void print_list(t_stack *n)
+{
+    while (n->next)
+    {
+        printf("%d ", n->nb);
+        n = n->next;
+    }
+}
 
 
-int main() {
-    char c[] = {1, 5, 3, 3, 8, 2, 4,20,1,9,3};
-    int i = 0;
-    int j = 0;
-    int k;
-    int z = 0;
-   int size = (int)ft_strlen(c) - 1;
 
-    //сортировка пузырьком
-//    while (size--)
-//    {
-//        j = 0;
-//        while (c[j + 1] != '\0')
-//        {
-//            if (c[j] > c[j + 1])
-//            {
-//                k = c[j];
-//                c[j] = c[j + 1];
-//                c[j + 1] = k;
-//            }
-//            j++;
-//        }
-//    }
-    //сортировка выбором
-//    while (c[i] != '\0')
-//    {
-//        j = i;
-//        while(c[j] !='\0')
-//        {
-//            if (c[j] < c[i])
-//            {
-//                k = c[i];
-//                c[i] = c[j];
-//                c[j] = k;
-//            }
-//            j++;
-//        }
-//        i++;
-//    }
-    //сортировка вставками
-//    while (i < size)
-//    {
-//        k = c[i];
-//        j = i - 1;
-//        while(j >= 0 && c[j] > k)
-//        {
-//            c[j + 1] = c[j];
-//            j--;
-//        }
-//        c[j + 1] = k;
-//        i++;
-//    }
-    while (z != (int)ft_strlen(c) - 1)
-        printf("%d ",(int)c[z++]);
+
+void push_stack(t_stack **head,int number)
+{
+    t_stack *new;
+
+    new = (t_stack *)malloc(sizeof(t_stack));
+
+    new->nb = number;
+    new->next = (*head);
+    (*head) = new;
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    t_stack *stack_a;
+    t_stack *stack_b;
+    t_stack *head;
+
+    head = (t_stack *)malloc(sizeof(t_stack));
+    i = 1;
+    if (argc >= 2)
+    {
+        while(argv[i])
+        {
+            push_stack(&head,ft_atoi(argv[i]));
+            i++;
+        }
+        print_list(head);
+    }
+    free(head);
     return (0);
 }
---
+++ push_swap.h
@@ -0,0 +1,14 @@
+
+
+#ifndef PUSH_SWAP_PUSH_SWAP_H
+#define PUSH_SWAP_PUSH_SWAP_H
+
+#include "./libft/libft.h"
+
+typedef struct s_stack
+{
+    int nb;
+    struct s_stack *next;
+}               t_stack;
+
+#endif //PUSH_SWAP_PUSH_SWAP_H
