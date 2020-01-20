2d7a5bba007df148fadf825acbe7c247cb37eacc
Bonebreaker Glinda <bglinda@mi-d6.21-school.ru>
2020-01-20 14:42:59 +0300

    swap first and last elements of list

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ void print_list(t_stack *n)
 {
     while (n)
     {
-        printf("%d ", n->nb);
+        ft_putnbr(n->nb);
+        write(1," ",1);
         n = n->next;
     }
 }
@@ -41,7 +42,6 @@ void append_stack(t_stack **head, int number)
 
 	new->nb = number;
 	new->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new;
@@ -65,27 +65,66 @@ void push_stack(t_stack **head,int number)
     (*head) = new;
 }
 
+t_stack *create_stack(t_stack **head,char **argv,int argc)
+{
+    t_stack *a;
+    int i;
+    int nb;
+
+    i = 1;
+    a = *head;
+    while(i < argc)
+    {
+        append_stack(&a,ft_atoi(argv[i]));
+        i++;
+    }
+    return (a);
+}
+
+void rotate_a(t_stack **a)
+{
+    t_stack *tmp;
+    t_stack **ptr;
+    t_stack *last;
+
+    tmp = *a;
+    ptr = &(*a)->next;
+    while (*ptr && (*ptr)->next != NULL)
+        ptr = &(*ptr)->next;
+    (*a) = (*ptr);
+    (*ptr) = tmp;
+
+    (*a)->next = (*ptr)->next;
+    (*ptr)->next = NULL;
+
+}
+
 int main(int argc, char **argv)
 {
-    int i;
     t_stack *a;
     t_stack *b;
     t_stack *head = NULL;
 
-    i = 1;
     if (argc >= 2)
     {
-        while(argv[i])
-        {
-			append_stack(&head,ft_atoi(argv[i]));
-            //push_stack(&head,ft_atoi(argv[i]));
-            i++;
-        }
+        a = create_stack(&head,argv,argc);
+        b = create_stack(&head,argv,argc);
     }
-	print_list(head);
+	print_list(a);
     printf("\n");
-    reverse_stack(&head);
-    print_list(head);
+    //swap_a(&a);
+    //reverse_stack(&a);
+    //swap_ss(&a,&b);
+    rotate_a(&a);
+    print_list(a);
+
+    printf("\n__________________\n");
+
+//    print_list(b);
+//    printf("\n");
+   // swap_a(&b);
+   // reverse_stack(&b);
+//    print_list(b);
     free(head);
     return (0);
 }
